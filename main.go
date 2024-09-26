073558d0d9f6decf8c1fdd350386414992feca0c
Эрдни Окунов <61590286+ok-42@users.noreply.github.com>
2024-09-26 22:52:22 +0300

    Add venv and full path to PS1
    Changes:
    - venv is added to PS1 if one is active
    - PS1 displays the rest of the path after the ignored prefix
    - PROMPT_COMMAND always sets PS1

--- main.go
+++ main.go
@@ -8,12 +8,34 @@ import (
 
 func main() {
 	prefixToIgnore := "/some/long/path"
+	lastIndex := strings.LastIndex(prefixToIgnore, "/")
+	prefixToIgnoreD := prefixToIgnore[:lastIndex+1]
 	currentDir := os.Args[1]
-	split := strings.Split(currentDir, "/")
-	finalDir := split[len(split)-1]
-	if strings.HasPrefix(currentDir, prefixToIgnore) {
-		fmt.Printf("\012\\[\\e[0;35m\\]\\t \\[\\e[0;32m\\]\\u@\\h \\[\\e[0;33m\\]%s\\[\\e[m\\]\\n$", finalDir)
-	} else {
-		fmt.Print("kek")
+
+	var result string = "\012"
+
+	// Add venv to PS1
+	venvName := os.Getenv("VIRTUAL_ENV")
+	var projectName string
+	if venvName != "" {
+		venvNameSplit := strings.Split(venvName, "\\")
+		projectName = venvNameSplit[(len(venvNameSplit) - 2)]
+		result += fmt.Sprintf("\\[\\e[1;34m\\](%s) ", projectName)
 	}
+
+	// Add current dir to PS1
+	var pathPlaceholder string
+	if strings.HasPrefix(currentDir, prefixToIgnore) {
+		if currentDir == prefixToIgnore {
+			pathPlaceholder = prefixToIgnore[lastIndex+1:]
+		} else {
+			pathPlaceholder, _ = strings.CutPrefix(currentDir, prefixToIgnoreD)
+		}
+	} else {
+		pathPlaceholder = "\\w"
+	}
+
+	// Print PS1           purple       time  green        user@host orange        pwd    erase colour
+	result += fmt.Sprintf("\\[\\e[0;35m\\]\\t \\[\\e[0;32m\\]\\u@\\h \\[\\e[0;33m\\]%s\\[\\e[m\\]\\n$", pathPlaceholder)
+	fmt.Println(result)
 }
--- update_ps1.sh
+++ update_ps1.sh
@@ -1,12 +1,7 @@
 update_ps1() {
     current_dir=$(pwd)
     result=$(/path/to/ps1.exe "$current_dir")
-    size=${#result}
-    if [ "$size" -le 20 ]; then
-        PS1='\012'$ORIG_PS1
-    else
-        PS1="$result "
-    fi
+    PS1="$result "
 }
 
 PROMPT_COMMAND=update_ps1
